30da3152f7de0c3efe3e2125a544337de83ef2e5
Purvi2401 <purvi2401binani@gmail.com>
2021-12-09 17:34:03 +0530

    view leaderboard/richest x people

--- main.py
+++ main.py
@@ -269,6 +269,34 @@ async def buy_this(user, item_name, amount):
     return [True, "Worked"]
 
 
+@client.command(aliases=["lb"])
+async def leaderboard(ctx, x=1):
+    users = await get_bank_data()
+    leader_board = {}
+    total = []
+    for user in users:
+        total_amount = int(users[user]["wallet"] + users[user]["bank"])
+        leader_board[total_amount] = user
+        total.append(total_amount)
+
+    total = sorted(total, reverse=True)
+
+    em = discord.Embed(title=f"Top {x} Richest People",
+                       description="This is decided on the basis of raw money in the bank and wallet", color=discord.Color(0xfa43ee))
+    index = 1
+    for amt in total:
+        id_ = leader_board[amt]
+        member = await client.fetch_user(id_)
+        name = member.name
+        em.add_field(name=f"{index}. {name}", value=f"{amt}",  inline=False)
+        if index == x:
+            break
+        else:
+            index += 1
+
+    await ctx.send(embed=em)
+
+
 async def open_account(user):
 
     users = await get_bank_data()
